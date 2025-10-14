70df929d62e902318d3efa10adac15856ee5d7fe
willianysouza2003@gmail.com <willianysouza2003@gmail.com>
2025-10-13 23:36:43 -0300

    Continuação do cadastro. Adicionado campos de itens

--- src/app/features/cadastros/opcoes/opcao/opcao.ts
+++ src/app/features/cadastros/opcoes/opcao/opcao.ts
@@ -8,6 +8,12 @@ import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
 import { ProductOption, ProductOptionItem } from '../../../../core/services/opcoes-service';
 import { FormsModule } from '@angular/forms';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { SelectButtonChangeEvent, SelectButtonModule } from 'primeng/selectbutton';
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputGroupModule } from 'primeng/inputgroup';
 
 @Component({
     selector: 'app-opcao',
@@ -19,14 +25,39 @@ import { FormsModule } from '@angular/forms';
         ConfirmPopupModule,
         DrawerModule,
         InputTextModule,
-        FormsModule
+        FormsModule,
+        ToggleButtonModule,
+        SelectButtonModule,
+        ToggleSwitchModule,
+        InputNumberModule,
+        InputGroupModule,
+        InputGroupAddonModule,
     ],
     templateUrl: './opcao.html',
     styleUrl: './opcao.css',
 })
 export class Opcao implements OnInit {
+    Math = window.Math;
+
+    multipleOptions: any[] = [
+        { label: 'Único', value: false },
+        { label: 'Múltiplo', value: true },
+    ];
+
+    autoSelectedOptions: any[] = [
+        { label: 'Não', value: 0 },
+        { label: 'Sim', value: 1 },
+    ];
+
+    descontoOptions: any[] = [
+        { label: 'Acrécimo', value: false },
+        { label: 'Desconto', value: true },
+    ];
+
     editingItem: boolean = false;
     editingItemIndex: number | null = null;
+
+    hasItemLimit: boolean = false;
 
     @Input() opcao: ProductOption | null = null;
 
@@ -37,7 +68,12 @@ export class Opcao implements OnInit {
     modelItem!: ProductOptionItem;
 
     ngOnInit(): void {
-        this.model = this.opcao ? { ...this.opcao } : { id: 0, label: '', items: [] };
+        this.model = this.opcao
+            ? {
+                  ...this.opcao,
+                  items: this.opcao.items ? [...this.opcao.items] : [],
+              }
+            : { id: 0, label: '', items: [] };
     }
 
     close() {
@@ -53,30 +89,62 @@ export class Opcao implements OnInit {
     }
 
     openEditItem(index: number | null) {
-        if(index == null){
-            this.modelItem = { id: 0, label: '', price: 0, autoSelectedCount: 0, isDiscount: false, isMultiple: false, maxSelectedCount: 0 };
-        }else{
+        if (index == null) {
+            this.modelItem = {
+                id: 0,
+                label: '',
+                price: 0,
+                autoSelectedCount: 0,
+                isDiscount: false,
+                isMultiple: false,
+                maxSelectedCount: 0,
+            };
+        } else {
             this.modelItem = { ...this.model.items[index] };
+            this.modelItem.maxSelectedCount = this.modelItem.maxSelectedCount ?? 0;
         }
 
         this.editingItem = true;
         this.editingItemIndex = index;
     }
 
-    closeEditItem(){
+    closeEditItem() {
         this.editingItem = false;
         this.editingItemIndex = null;
         this.modelItem = null!;
     }
 
     editItem() {
-        if(this.editingItemIndex == null){
+        let model: ProductOptionItem = {
+            id: 0,
+            label: this.modelItem.label,
+            price: this.modelItem.price ?? 0,
+            autoSelectedCount: this.modelItem.autoSelectedCount,
+            isDiscount: this.modelItem.isDiscount,
+            isMultiple: this.modelItem.isMultiple,
+            maxSelectedCount: this.hasItemLimit ? this.modelItem.maxSelectedCount : null,
+        };
 
-        }else{
-
+        if (this.editingItemIndex == null) {
+            // Sempre cria novo array
+            this.model.items = [...this.model.items, model];
+        } else {
+            // Cria novo array com item substituído
+            this.model.items = this.model.items.map((item, index) =>
+                index === this.editingItemIndex ? model : item
+            );
         }
 
         this.editingItem = false;
         this.editingItemIndex = null;
+    }
+
+    onAutoSelectedInput() {
+        if (
+            this.hasItemLimit &&
+            this.modelItem.autoSelectedCount > this.modelItem.maxSelectedCount!
+        ) {
+            this.modelItem.autoSelectedCount = this.modelItem.maxSelectedCount!;
+        }
     }
 }
